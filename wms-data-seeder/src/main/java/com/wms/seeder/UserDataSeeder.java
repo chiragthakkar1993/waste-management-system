25df2308a4f965643b6d66af6e04883eb8c1f68d
chirag <chirag.thakkar1993@gmail.com>
2020-05-12 03:51:16 +0530

    users fetchAll minor bug fixed (Null Pointer Exception)
    Signed-off-by: chirag <chirag.thakkar1993@gmail.com>

--- wms-data-seeder/src/main/java/com/wms/seeder/UserDataSeeder.java
+++ wms-data-seeder/src/main/java/com/wms/seeder/UserDataSeeder.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
+import com.wms.entity.Customer;
 import com.wms.entity.User;
 
 /**
@@ -30,9 +31,9 @@ public class UserDataSeeder {
 	public void setup() {
 		mongoTemplate.dropCollection(User.class);
 		User chirag = User.builder().email("chirag.thakkar1993@gmail.com").firstName("chirag").lastName("Thakkar")
-				.mobile("9727432368").build();
+				.mobile("9727432368").role(new Customer()).build();
 		User gaurav = User.builder().email("gaurav@gmail.com").firstName("gaurav").lastName("mittal")
-				.mobile("1234567890").build();
+				.mobile("1234567890").role(new Customer()).build();
 
 		this.mongoTemplate.insertAll(Arrays.asList(chirag, gaurav));
 
